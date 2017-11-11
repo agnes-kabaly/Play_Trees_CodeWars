0c4c82508b8c611a840ac47589a0314d8315f09e
agnes-kabaly <alicet.woods@gmail.com>
2017-11-11 23:11:00 +0100

    Binary Tree with Main

---
+++ src/BinaryTree.java
@@ -0,0 +1,14 @@
+public class BinaryTree {
+
+    private TreeNode root;
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.setRoot(new TreeNode(5));
+        binaryTree.getRoot().setLeft(new TreeNode(-22));
+        binaryTree.getRoot().setRight(new TreeNode(11));
+        binaryTree.getRoot().getLeft().setLeft(new TreeNode(9));
+        binaryTree.getRoot().getLeft().setRight(new TreeNode(50));
+        binaryTree.getRoot().getRight().setLeft(new TreeNode(9));
+        binaryTree.getRoot().getRight().setRight(new TreeNode(2));
 
+        MaxNodeValue maxNodeValue = new MaxNodeValue();
+        System.out.println(maxNodeValue.findMax(binaryTree.getRoot()));
     }
 
 
